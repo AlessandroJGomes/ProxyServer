a894cbe584ecc154d219e7b5e4b040954be403ec
Alessandro Gomes <alessandro.gomes@samtrevano.ch>
2018-10-02 16:28:21 +0200

    Diario + Pagina web + Media + database + schema E-R

---
+++ Codici/Database/databaseAccessi.sql
@@ -0,0 +1,20 @@
+create database IF NOT EXISTS informatica;
+
+use informatica;
+
+create table IF NOT EXISTS classi(
+Anno_Classe int(1) NOT NULL,
+Id_Classe varchar(2) NOT NULL,
+primary key(Anno_Classe, Id_Classe)
+);
+
+create table IF NOT EXISTS alunni(
+Nome varchar(30) NOT NULL,
+Cognome varchar(30) NOT NULL,
+Stato_Accesso	bool NOT NULL default false,
+Youtube bool NOT NULL default false,
+Anno_Classe int(1) NOT NULL,
+Id_Classe varchar(2) NOT NULL,
+primary key(Nome, Cognome),
+FOREIGN KEY (Anno_Classe, Id_Classe) REFERENCES classi(Anno_Classe, Id_Classe) on delete cascade on update cascade
+);
